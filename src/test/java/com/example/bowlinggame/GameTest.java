bc6189bb36ed0410e049a60e81ae6f1289713c31
albindubech <albin.dubech@hotmail.com>
2022-01-17 18:17:08 +0100

    Change initialization of GameTest.java and removed repeated code with new method

--- src/test/java/com/example/bowlinggame/GameTest.java
+++ src/test/java/com/example/bowlinggame/GameTest.java
@@ -5,23 +5,29 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class GameTest {
-    Game game = new Game();
+    Game game;
+
+    GameTest() {
+        game = new Game();
+    }
 
     @Test
     void fullGutterGameShouldReturnZero(){
-        for (int i = 0; i < 20; i++) {
-            game.roll(0);
-        }
+        fullSeries(20,0);
 
         assertEquals(0, game.score());
     }
 
     @Test
     void fullGameOfOnePinKnockedShouldReturnTwenty(){
-        for (int i = 0; i < 20; i++) {
-            game.roll(1);
-        }
+        fullSeries(20,1);
 
         assertEquals(20, game.score());
+    }
+
+    private void fullSeries(int rolls, int pins) {
+        for (int i = 0; i < rolls; i++) {
+            game.roll(pins);
+        }
     }
 }
