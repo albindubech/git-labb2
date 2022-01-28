fa6c61344d91a5531c25350e875729e46657cb17
albindubech <albin.dubech@hotmail.com>
2022-01-28 16:14:39 +0100

    Add functionality to EmployeeRepositoryStub

--- src/test/java/com/example/EmployeeRepositoryStub.java
+++ src/test/java/com/example/EmployeeRepositoryStub.java
@@ -3,10 +3,15 @@ package com.example;
 import java.util.List;
 
 public class EmployeeRepositoryStub implements EmployeeRepository{
+    List<Employee> employees;
+
+    public EmployeeRepositoryStub(List<Employee> employees) {
+        this.employees = employees;
+    }
 
     @Override
     public List<Employee> findAll() {
-        return null;
+        return employees;
     }
 
     @Override
