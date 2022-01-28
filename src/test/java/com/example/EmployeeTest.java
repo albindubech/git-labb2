fa2599c0f4aefe7c1f773a60a60ae0465bac2db7
albindubech <albin.dubech@hotmail.com>
2022-01-28 15:38:49 +0100

    Add test for Employee getId method

--- src/test/java/com/example/EmployeeTest.java
+++ src/test/java/com/example/EmployeeTest.java
@@ -1,4 +1,14 @@
 package com.example;
 
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
 public class EmployeeTest {
+    Employee employee = new Employee("1", 20_000.0);
+
+    @Test
+    void getIdShouldReturn1() {
+        assertThat(employee.getId()).isEqualTo("1");
+    }
 }
