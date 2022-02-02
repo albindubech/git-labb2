faa9f08c80beae33dcce4dc671696b91cce3b532
albindubech <albin.dubech@hotmail.com>
2022-02-02 22:19:10 +0100

    Create EmployeeRepositoryMemory which implements EmployeeRepository

---
+++ src/main/java/com/example/EmployeeRepositoryMemory.java
@@ -0,0 +1,16 @@
+package com.example;
+
+import java.util.List;
+
+public class EmployeeRepositoryMemory implements EmployeeRepository {
+
+    @Override
+    public List<Employee> findAll() {
+        return null;
+    }
+
+    @Override
+    public Employee save(Employee e) {
+        return null;
+    }
+}
