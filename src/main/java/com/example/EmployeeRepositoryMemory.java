1f8079889a7cae41400e2ad890b4fb30cecb5b80
albindubech <albin.dubech@hotmail.com>
2022-02-02 22:20:58 +0100

    Implement correct functionality of the EmployeeRepositoryMemory version

--- src/main/java/com/example/EmployeeRepositoryMemory.java
+++ src/main/java/com/example/EmployeeRepositoryMemory.java
@@ -4,13 +4,25 @@ import java.util.List;
 
 public class EmployeeRepositoryMemory implements EmployeeRepository {
 
+    List<Employee> employees;
+
+    public EmployeeRepositoryMemory(List<Employee> employees) {
+        this.employees = employees;
+    }
+
     @Override
     public List<Employee> findAll() {
-        return null;
+        return employees;
     }
 
     @Override
     public Employee save(Employee e) {
-        return null;
+
+        employees.stream()
+                .filter(employee -> employee.getId().equals(e.getId()))
+                .findFirst()
+                .ifPresentOrElse(employee -> employees.set(employees.indexOf(employee),e),() -> employees.add(e));
+
+        return e;
     }
 }
