f0803fa36f69c5a0a1f814fea7865a0842532d48
albindubech <albin.dubech@hotmail.com>
2022-02-02 22:22:51 +0100

    Create EmployeeRepositoryMemoryTest and first test for when it is empty

---
+++ src/test/java/com/example/EmployeeRepositoryMemoryTest.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+
+class EmployeeRepositoryMemoryTest {
+
+    List<Employee> employees = new ArrayList<>(List.of(
+            new Employee("123", 100.0),
+            new Employee("456", 99.0),
+            new Employee("789", 101.0)));
+
+    EmployeeRepositoryMemory employeeRepository = new EmployeeRepositoryMemory(employees);
+
+
+    @Test
+    void emptyRepositoryShouldReturnZero() {
+        EmployeeRepositoryMemory employeeRepository = new EmployeeRepositoryMemory(List.of());
+
+        var result = employeeRepository.findAll();
+
+        assertThat(result).size().isEqualTo(0);
+    }
+}
